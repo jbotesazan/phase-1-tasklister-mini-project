549820a7bd9fe8f6fd1c2a89b2f97871674d31c8
Jason Botesazan <jbotesazan@gmail.com>
2021-12-01 13:57:07 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const form = document.getElementById('create-task-form')
+    const taskList = document.querySelector('#tasks')
+    const taskData = document.querySelector('#new-task-description')
+
+    form.addEventListener('submit', (e) => {
+      const task = taskData.value;
+      e.preventDefault();
+      const li = document.createElement('li');
+      li.innerText = task;
+      const button = document.createElement('button');
+      button.innerText = "x";
+      li.appendChild(button);
+      taskList.appendChild(li);
+      taskData.value = '';
+      button.addEventListener('click', (e) => {
+        e.target.parentNode.remove()
+      })
+    })
+    
 });
